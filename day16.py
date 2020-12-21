e5a22982e07948dc897a557ceed0d86dbc22554d
Jason Hussey <jphussey05@gmail.com>
2020-12-21 11:41:46 -0700

    day16 sucked!

--- day16.py
+++ day16.py
@@ -1,8 +1,13 @@
 from copy import deepcopy
 from pprint import pprint
 
-def remove_element(ele):
-    pass
+def remove_element(ele, template):
+    for t in template:
+        if ele in t:
+            t.remove(ele)
+
+    return template
+
 
 def template_check(template):
     for t in template:
@@ -10,18 +15,20 @@ def template_check(template):
             return False    
     return True
 
+
 def your_tix_multiply(correct_perm, your_tix):
     answer = 1
-
     return answer
 
 
-def validate_tix(ticket, valid_nums):
+def validate_tix(ticket, valid_nums, all_valids):
     for val in ticket:
-        for k, v in valid_nums.items():
-            if val in v:
-                return True
-    return False
+        if val in all_valids:
+            continue
+        else:
+            return False
+
+    return True
 
 
 if __name__ == "__main__":
@@ -31,7 +38,6 @@ if __name__ == "__main__":
     valid_nums = {}
     valid_tix = set()
 
-    print(f'Building valid_nums')
     for idx, line in enumerate(contents):
         
         if not line:  # skip blank lines
@@ -49,38 +55,43 @@ if __name__ == "__main__":
 
         valid_nums[colon_sep[0]] = range1_nums + range2_nums
     
-    
+    all_valids = []
+    for v in valid_nums.values():
+        all_valids.extend(v)
+    all_valids = set(all_valids)
+
     your_tix = list(map(int, contents[idx].split(',')))
 
     template = [list(valid_nums.keys()) for _ in range(len(valid_nums.keys()))]
     nearby_tix = [list(map(int, line.split(','))) for line in contents[idx+3:]]
 
 
-    valid_tix = [t for t in nearby_tix if validate_tix(t, valid_nums)]
+    valid_tix = [t for t in nearby_tix if validate_tix(t, valid_nums, all_valids)]
                 
-    # print(valid_nums)
-
-    # print(template)
-    # print(nearby_tix)
-    print(valid_tix)
     for t in valid_tix:
-        # print(f'Looking at {t}')
-        for idx, value in enumerate(t):
-            bad_keys = []
-            tmp_keys = deepcopy(template[idx])
-            for key in tmp_keys:
-                
-                # print(f' Checking {value} in {key}')
-                if value in valid_nums[key]:
-                    continue
-                else:
-                    # print(f'{value} is not in {key}')
-                    bad_keys.append(key)
+            # print(f'Looking at {t}')
+            for idx, value in enumerate(t):
+                bad_keys = []
+                tmp_keys = deepcopy(template[idx])
+                for key in tmp_keys:
+                    
+                    # print(f' Checking {value} in {key}')
+                    if value in valid_nums[key]:
+                        continue
+                    else:
+                        # print(f'{value} is not in {key}')
+                        bad_keys.append(key)
 
-            for bk in bad_keys:
-                # print(f'Removing {bk} from {template[idx]}')
-                template[idx].remove(bk)
-
+                for bk in bad_keys:
+                    # print(f'Removing {bk} from {template[idx]}')
+                    template[idx].remove(bk)
 
   
     pprint(template)
+
+    answer = 1
+    departures = [0, 8, 10, 11, 17, 19]
+    for d in departures:
+        answer *= your_tix[d]
+
+    print(answer)
