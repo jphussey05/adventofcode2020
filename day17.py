c97283a9246921de1e3f1c1be4efbd871a8275d5
Jason Hussey <jphussey05@gmail.com>
2020-12-22 12:01:46 -0700

    timing for day17

--- day17.py
+++ day17.py
@@ -2,7 +2,7 @@ from math import sqrt
 from itertools import product
 from copy import deepcopy
 from collections import Counter
-
+from time import time
 
 class Point(object):
     def __init__(self, x, y, z, w,state='inactive'):
@@ -42,7 +42,7 @@ def count_active_neighbors(point, actives, database):
 if __name__ == "__main__":
     with open('day17.txt') as fin:
         contents = [line.strip() for line in fin.readlines()]
-
+    t0 = time()
     # initial population of database with active Points
     database = {}
     for y, row in enumerate(contents):
@@ -51,9 +51,10 @@ if __name__ == "__main__":
                 database[(x, -y, 0, 0)] = Point(x, -y, 0, 0, 'active')
 
     print(f'Initial number of actives is {len(database)}')
-
-
+    deltas = 0
+    t1 = time()
     for n in range(6):
+        t2 = time()
         adj_cntr = Counter()
         next_db = {}
         for key, point in database.items():         # look at all active points
@@ -71,5 +72,11 @@ if __name__ == "__main__":
             if adj_cntr[inactive] == 3:
                 next_db[inactive] = Point(inactive[0], inactive[1], inactive[2], inactive[3], state='active')
 
-        print(f'Number active after {n+1} rounds is {len(next_db)}')
+        # print(f'Number active after {n+1} rounds is {len(next_db)}')
         database = deepcopy(next_db)
+        t3 = time()
+        deltas += (t3-t2)
+    t4 = time()
+    print(f'Time to populate initial = {t1 - t0}')
+    print(f'Average loop time = {deltas / 6}')
+    print(f'Total time = {t4-t0}')
